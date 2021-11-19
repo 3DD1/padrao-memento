177712e7f97f904d2c56cd9cbe742730d345c4a9
Edmilson <scovinowork@gmail.com>
2021-11-19 09:24:41 -0300

    terceiro teste passou

--- src/test/java/PessoaTest.java
+++ src/test/java/PessoaTest.java
@@ -20,4 +20,15 @@ class PessoaTest {
         pessoa.restaurarEstado(0);
         assertEquals(PessoaEstadoSolteiro.getInstance(), pessoa.getEstado());
     }
+    @Test
+    void deveRetornarEStadoAnterior(){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setEstado(PessoaEstadoSolteiro.getInstance());
+        pessoa.setEstado(PessoaEstadoNamorando.getInstance());
+        pessoa.setEstado(PessoaEstadoNoivando.getInstance());
+        pessoa.setEstado(PessoaEstadoCasado.getInstance());
+        pessoa.setEstado(PessoaEstadoSeparado.getInstance());
+        pessoa.restaurarEstado(2);
+        assertEquals(PessoaEstadoNoivando.getInstance(), pessoa.getEstado());
+    }
 }
